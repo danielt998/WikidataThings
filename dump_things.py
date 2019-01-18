fc605d8ee271d4b294b7fb85a3f482bb19a99f39
Daniel Thomas <danielt998@gmail.com>
2019-01-18 20:13:53 +0000

    make code a little more generic and add category exclusion to generate_statements

--- dump_things.py
+++ dump_things.py
@@ -2,46 +2,43 @@ from utils import *
 
 DELIMITER='\t'
 #FILE='/media/dtm/Seagate Expansion Drive/wikidata-20181231-all.json'
-FILE='/media/dtm/wikidata/first1000lines.json'
-OUTPUT_FILE_PREFIX='v3_output/'
-OUTPUT_FILE_SUFFIX='_labels_descs_v3.tsv'
-count=0
-#langs=['ceb', 'cy', 'en', 'fr', 'de', 'zh-cn', 'zh', 'zh-hans', 'zh-hant','zh-tw', 'es']
-langs=['sv', 'war']
-
-files={}
-for lang in langs:
-    files[lang] = open(OUTPUT_FILE_PREFIX + lang + OUTPUT_FILE_SUFFIX, 'a+')
-
+FILE = '/media/dtm/wikidata/first1000lines.json'
 
 def do_filtering(lang_str, desc_str):
     return '(' in lang_str
 
+def dump(filter=do_filtering, folder_name='default_output', langs=['en','cy']):
+    OUTPUT_FILE_SUFFIX='_labels_descs_v3.tsv'
+    count=0
 
-with open(FILE) as infile:
-    for line in infile:
-        if line.startswith("[") or line.startswith("]"):
-            continue
-            if line.startswith("["):
-                line = line[1:]
-            if line.startswith("]"):
-                continue
-        i = ''
-        line = line.rstrip()
-        if line.endswith(","):
-            i = json.loads(line[:-1])
-        else:
-            i = json.loads(line)#TODO:test this works on the last line...
-        if i != '':
-            continue
-        output_string = ''
-        for lang in langs:
-            lang_str = getLangString(lang, i)
-            desc_str = getDesc(lang, i)
-            if not do_filtering(lang_str, desc_str):
-                continue
-            output_string = output_string + getID(i) + DELIMITER + lang_str + DELIMITER + desc_str
-            files[lang].write(output_string + '\n')
+    files={}
+    for lang in langs:
+        files[lang] = open(folder_name + "/" + lang + OUTPUT_FILE_SUFFIX, 'a+')
 
-        #print(str+getDesc('en',i)+DELIMITER+getTypes(line)+DELIMITER+getCountry(line)+DELIMITER+getRegions(line))
-            #print(str)
+    with open(FILE) as infile:
+        for line in infile:
+            if line.startswith("[") or line.startswith("]"):
+                continue
+                if line.startswith("["):
+                    line = line[1:]
+                if line.startswith("]"):
+                    continue
+            i = ''
+            line = line.rstrip()
+            if line.endswith(","):
+                i = json.loads(line[:-1])
+            else:
+                i = json.loads(line)#TODO:test this works on the last line...
+            if i != '':
+                continue
+            output_string = ''
+            for lang in langs:
+                lang_str = getLangString(lang, i)
+                desc_str = getDesc(lang, i)
+                if not filter(lang_str, desc_str):
+                    continue
+                output_string = output_string + getID(i) + DELIMITER + lang_str + DELIMITER + desc_str
+                files[lang].write(output_string + '\n')
+
+            #print(str+getDesc('en',i)+DELIMITER+getTypes(line)+DELIMITER+getCountry(line)+DELIMITER+getRegions(line))
+                #print(str)
--- generate_statements.py
+++ generate_statements.py
@@ -1,17 +1,25 @@
 import re
 import sys
 
+category_translations=['Category', 'Kaarangay', 'Categori', 'Kategori']
+def is_probably_category(line):
+    for translation in category_translations:
+        if translation in line:
+            return True
+    return False
+
+
 #WARNING: Do not use on all items, only on specific ones
 #TODO:find a solution to the lat/long thing when it comes to doing descs
 #for now, maybe we should exclude all of them, as the lat/lon may be used in cases where the rest of the desc is the same
-LANG = sys.argv[2]#'ceb'
 #FILE = 'ceb_labels_descs_v2.tsv'
 FILE = sys.argv[1]
-new_desc_regex = re.compile("\((.*)\)")
+LANG = sys.argv[2]#'ceb'
 FILTER_WORD = sys.argv[3]
+new_desc_regex = re.compile("\((.*)\)")
 with open(FILE) as infile:
     for line in infile:
-        if FILTER_WORD and FILTER_WORD in line:
+        if FILTER_WORD and FILTER_WORD in line and not is_probably_category(line):
             segs = line.split('\t')
             item = segs[0]
             label = re.sub(' \(.*\)', '', segs[1])
